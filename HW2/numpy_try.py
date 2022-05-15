b30b5df0cad68a0b35cc7b6e1051ac3c65c3acb3
federicochiarello <federicochiarello99@gmail.com>
2022-05-16 00:41:28 +0200

    numpy optimization

--- HW2/numpy_try.py
+++ HW2/numpy_try.py
@@ -2,13 +2,11 @@ import sys
 import os
 import time
 import math
-from scipy.spatial import distance_matrix
 import numpy as np
 
-####################################################################
+
 import pandas as pd
 import matplotlib.pyplot as plt
-####################################################################
 
 
 def readVectorsSeq(filename):
@@ -17,34 +15,39 @@ def readVectorsSeq(filename):
     return result
     
     
-def euclidean(point1,point2):
-    res = 0
-    for i in range(len(point1)):
-        diff = (point1[i]-point2[i])
-        res +=  diff*diff
-    return math.sqrt(res)
+# def euclidean(point1,point2):
+#     res = 0
+#     for i in range(len(point1)):
+#         diff = (point1[i]-point2[i])
+#         res +=  diff*diff
+#     return math.sqrt(res)
 
 
-# def dist_matrix(P):
-#     P_array = np.asarray(P)
-#     dist_mat = distance_matrix(P_array, P_array)
-#     print(dist_mat)
+def euclidean_distance_matrix(x,y):
+    diff = np.expand_dims(x, axis=1) - np.expand_dims(y, axis=0)
+    distance_matrix = np.sqrt(np.sum(diff ** 2, axis=-1))
+    return distance_matrix
 
 
 def SeqWeightedOutliers(P,W,k,z,alpha=0):
 
+    # Precomputed distances
+    P_array = np.asarray(P)
+    dist_mat = euclidean_distance_matrix(P_array, P_array)
+
     # Compute r_min
-    P_array = np.asarray(P[:k+z+1])
-    dist_mat = distance_matrix(P_array, P_array)
-    r = min(dist_mat[np.nonzero(dist_mat)]) /2
+    r_min_mat = dist_mat[:k+z+1, :k+z+1]
+    r = min(r_min_mat[np.nonzero(r_min_mat)]) /2
 
     initial_r = r
     n_guesses = 1
+
+    n_points = len(P)
     
     while(True):
 
-        # P.copy() needed to avoid aliasing
-        Z = P.copy()
+        Z = np.ones(n_points, dtype=bool)
+        # Z = P.copy()
         S = []
         W_z = sum(W)
 
@@ -52,52 +55,36 @@ def SeqWeightedOutliers(P,W,k,z,alpha=0):
             max = 0
             newcenter = ()
 
-            # DUBBIO: x in P o Z
-            # stesso risultato, Z leggermente più veloce
-            for x in P:
-                # lista di indici
-                B_z_center = []
+            for x in range(n_points):
+                # row x from distance matrix (distances between x and all points in P)
+                x_row_dist = dist_mat[x]
+                
+                # distances <= (1+2*alpha)*r
+                # including distances with points that might not be in Z
+                center = x_row_dist <= (1+2*alpha)*r
 
-                # B_z(x,(1+2*alpha)*r)
-                for y in range(len(Z)):
-                    if(euclidean(x,Z[y]) <= (1+2*alpha)*r):
-                        B_z_center.append(y)
+                B_z_center = np.logical_and(center, Z)
 
-                ball_weight = 0
-                for i in B_z_center:
-                    ball_weight += W[i]
+                ball_weight = np.sum(W[B_z_center])
+
                 if ball_weight > max:
                     max = ball_weight
                     newcenter = x
-                # print(f'iteration {x}:\t', B_z_center, '\t', ball_weight, '\t', newcenter)
 
-            S.append(newcenter)
-            # print('\nS:\t\t',S)
+            S.append(P[newcenter])
 
-            # B_z(newcenter,(3+4*alpha)*r)
-            B_z_outlier = []
-            for y in range(len(Z)):
-                if(euclidean(newcenter,Z[y]) <= (3+4*alpha)*r):
-                    B_z_outlier.append(y)
+            newcenter_row_dist = dist_mat[newcenter]
+            center = newcenter_row_dist <= (3+4*alpha)*r
+            B_z_outlier = np.logical_and(center, Z)
 
-            # print(f'B_z out:\t', B_z_outlier)
-            # print(f'\nZ:\t\t',Z,'\nW_z:\t\t', W_z)
-
-            for y in range(len(B_z_outlier)-1,-1,-1):
-                Z.pop(B_z_outlier[y])
-                W_z -= W[B_z_outlier[y]]
-
-            # print(f'\nZ:\t\t',Z,'\nW_z:\t\t', W_z)    
-        # print('\nUSCITI DA WHILE INTERNO\n')
+            Z = np.logical_xor(B_z_outlier, Z)
+            W_z -= np.sum(W[B_z_outlier])
         
         if W_z <= z:
             print(f'Initial guess = {initial_r}\nFinal guess = {r}\nNumber of guesses = {n_guesses}')
 
             # Plot Points
-
-            print(P)
-
-            plot_cluster(P,S,Z,k,z,r)
+            plot_cluster(P,S,P_array[Z],k,z,r)
 
             return S
         else:
@@ -108,31 +95,11 @@ def SeqWeightedOutliers(P,W,k,z,alpha=0):
 def ComputeObjective(P,S,z):
     P_array = np.asarray(P)
     S_array =  np.asarray(S)
-    dist_mat = distance_matrix(P_array, S_array)
+    dist_mat = euclidean_distance_matrix(P_array, S_array)
     min_dist = dist_mat.min(1).tolist()
-    # print(min_dist)
-    # print(dist_mat)
     for x in range(z):
         min_dist.remove(max(min_dist))
     return max(min_dist)
-
-
-# def ComputeObjective(P,S,z):
-#     P_array = np.asarray(P)
-#     S_array = np.asarray(S)
-
-#     dist_mat = distance_matrix(P_array, S_array)
-
-#     flat_dist_mat = dist_mat.flatten()
-
-#     # min_dist = dist_mat.min(1).tolist()
-#     print(dist_mat)
-#     # print(dist_mat)
-#     # for x in range(z):
-#     #     min_dist.remove(max(min_dist))
-#     # print(max(min_dist))
-
-#     return 69
 
 
 ####################################################################
@@ -181,7 +148,7 @@ def main():
     print(f'Input size n = {n}\nNumber of centers k = {k}\nNumber of outliers z = {z}')
 
     # unit weigtht list
-    weights = [1] * n
+    weights = np.ones(n)
 
     start = time.time()
     solution = SeqWeightedOutliers(inputPoints,weights,k,z,0)
